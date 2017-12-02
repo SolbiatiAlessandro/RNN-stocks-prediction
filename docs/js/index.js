bc071f6adb6a3701545096cc913c0cfb4d4a88e9
Solbiati Alessandro <alexsolbiati@hotmail.it>
2017-12-02 01:04:46 +0100

    Update index.js

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,7 +1,4 @@
-var script = document.createElement('script');
-script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+
 var canvas = document.getElementById('container');
 var clone = document.getElementById('blurCanvasBottom');
 
